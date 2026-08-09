3da0012f8fd81e032277393f52f674e10641562a
xuanlockun <xuanlockun594@gmail.com>
2026-08-09 13:22:39 +0700

    feat: add cve

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,48 +42,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 async function fetchGoogleSheetData() {
-    const ctfUrl = "https://opensheet.elk.sh/1OJ1gs4Md9wFiaMFOED06MUG1KgxuZkmzzhRvYENMCkQ/CTF_2025";
     const rankUrl = "https://opensheet.elk.sh/1OJ1gs4Md9wFiaMFOED06MUG1KgxuZkmzzhRvYENMCkQ/Rank";
 
     try {
-        const [ctfResponse, rankResponse] = await Promise.all([
-            fetch(ctfUrl).then(res => res.json()),
-            fetch(rankUrl).then(res => res.json())
-        ]);
-
-        updateCTFTable(ctfResponse);
+        const rankResponse = await fetch(rankUrl).then(res => res.json());
 
         updateRankInfo(rankResponse);
 
     } catch (error) {
         console.error("Lỗi khi tải dữ liệu từ Google Sheets:", error);
     }
-}
-
-function updateCTFTable(data) {
-    const tableBody = document.querySelector("#dataTable tbody");
-    tableBody.innerHTML = "";
-
-    let totalRating = 0;
-    data.forEach(event => {
-        totalRating += parseFloat(event["Rating Points"]);
-
-        const row = document.createElement("tr");
-        row.innerHTML = `
-            <td>${event.Place}</td>
-            <td class="event"><a href="${event.Link}" target="_blank"><i class="fa-solid fa-flag"></i> ${event.Event}</a></td>
-            <td>${parseFloat(event["CTF Points"]).toFixed(4)}</td>
-            <td>${parseFloat(event["Rating Points"]).toFixed(3)}</td>
-        `;
-        tableBody.appendChild(row);
-    });
-
-    const totalRow = document.createElement("tr");
-    totalRow.innerHTML = `
-        <td colspan="3" style="font-weight: bold;">Total Rating Points:</td>
-        <td style="font-weight: bold;">${totalRating.toFixed(3)}</td>
-    `;
-    tableBody.appendChild(totalRow);
 }
 
 function updateRankInfo(rankData) {
